727063eeacd78a079ab96a7189167f164948e16b
rossdev-noonan <automation@noonan.com.au>
2026-07-15 10:02:50 +0800

    2026-07-15_match-updated-trust-bar-design-with-icons_changesdone

--- components/public/home/HomeFeatureBar.tsx
+++ components/public/home/HomeFeatureBar.tsx
@@ -1,27 +1,34 @@
-import { Fragment } from "react";
-
 import type { Dictionary } from "@/lib/i18n/types";
 
 interface HomeFeatureBarProps {
   dictionary: Dictionary;
 }
 
+const ICONS = [
+  "/images/homepage/icons/trust-generations.svg",
+  "/images/homepage/icons/trust-years.svg",
+  "/images/homepage/icons/trust-countries.svg",
+  "/images/homepage/icons/trust-dojos.svg",
+];
+
 export function HomeFeatureBar({ dictionary }: HomeFeatureBarProps) {
   return (
     <section className="mx-auto max-w-7xl px-4 py-20 sm:px-6 lg:px-10">
-      <div className="flex flex-col gap-8 border border-[#C8A24A] bg-white px-6 py-8 sm:flex-row sm:items-center sm:justify-center sm:gap-[40px] sm:px-10 sm:py-9 lg:px-[80px] lg:py-[48px]">
+      <div className="flex flex-wrap justify-center gap-3">
         {dictionary.home.trustBar.map((item, index) => (
-          <Fragment key={item.label}>
-            {index > 0 && (
-              <div className="hidden h-[58px] w-0 border-l border-[#C8A24A] sm:block" />
-            )}
-            <div className="flex-1 text-center">
-              <p className="font-heading text-[48px] leading-none font-bold text-[#C1121F]">
-                {item.value}
-              </p>
-              <p className="mt-1 text-[14px] font-semibold text-[#4B5563]">{item.label}</p>
-            </div>
-          </Fragment>
+          <div
+            key={item.label}
+            className="flex h-[163px] w-[193px] flex-col items-center justify-center border border-[#E5E7EB] bg-white px-2 py-2.5 text-center"
+          >
+            {/* eslint-disable-next-line @next/next/no-img-element -- small
+                decorative vector icons, no benefit from next/image's raster
+                optimization pipeline and SVGs aren't optimized by default */}
+            <img src={ICONS[index]} alt="" width={35} height={33} className="h-[33px] w-auto" />
+            <p className="font-heading -mt-1 text-[60px] leading-[86px] font-bold text-[#C1121F]">
+              {item.value}
+            </p>
+            <p className="-mt-1 text-[14px] font-semibold text-[#1F2937]">{item.label}</p>
+          </div>
         ))}
       </div>
     </section>
